9d56f84c360221c9758c6d5fd70a89fb4aed9844
muk7777 <muk8620@gmail.com>
2023-04-13 20:33:00 +0900

    명령어 입력

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,19 @@ public class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		
+		while (true) {
+			System.out.println("명령어) ");
+			String cmd = sc.nextLine();
+			
+			
+			if (cmd.equals("exit")) {
+				break;
+			}
+		}
+		
+		sc.close();
+		
 		System.out.println("== 프로그램 끝 ==");
 	}
 }
